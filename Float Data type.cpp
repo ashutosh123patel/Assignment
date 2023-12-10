fad2dae62fd24d934b59b8407e80bc34acfdfcf8
ashutosh123patel <145385513+ashutosh123patel@users.noreply.github.com>
2023-12-11 00:00:40 +0530

    Add files via upload

---
+++ Float Data type.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  int main ()
+  {
+	  float x = 587;
+	  float y = 29;			 
+	  printf("%f",x+y);
+      printf("\n%f",x-y);
+      printf("\n%f",x*y);
+	  printf("\n%f",x/y);
+	  return 0 ;
+	  }
